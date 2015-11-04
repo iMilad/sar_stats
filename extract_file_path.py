4ef7ecf52f2860a386eaa7f59310a6e06400ac6d
Milad Khakpour <milad.khakpour@gmail.com>
2015-11-04 14:51:56 +0100

    extract date from file path save it as csv file

--- extract_file_path.py
+++ extract_file_path.py
@@ -3,6 +3,10 @@ import pandas as pd
 df = pd.read_csv('file.txt', header=None)
 df.columns = ['paths']
 # pandas string method extract takes a regex
-s = df['paths'].str.extract('(\d{4})(\d{2})')
+df = df['paths'].str.extract('ASA_\w{3}_(.{6})(\d{4})(\d{2})(\d{2})')
 
-print s
+# Columns 2,3,4
+df = df.ix[:, 1:4]
+
+# Save it as a csv file
+df.to_csv('out.csv', sep=',', header=False, index=False)
